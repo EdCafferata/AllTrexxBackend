d76669e39e47b45ddfaee477d95c427cf2a87192
EdCafferata <cafferata@live.nl>
2022-11-02 14:45:59 +0100

    first setup

---
+++ src/main/java/info/AllTrexx/AllTrexx/AllTrexxApplication.java
@@ -0,0 +1,13 @@
+package info.AllTrexx.AllTrexx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllTrexxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllTrexxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/AllTrexx/AllTrexx/AllTrexxApplicationTests.java
@@ -0,0 +1,13 @@
+package info.AllTrexx.AllTrexx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllTrexxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
